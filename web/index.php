1824f7e13e2b83b257a4379ea60cc798305a83b0
何冰 <hebing@hebingdeMacBook-Air.local>
2018-12-29 16:18:52 +0800

    新增.env配置 优化编译速度

--- web/index.php
+++ web/index.php
@@ -1,14 +1,2 @@
 <?php
-$http = new swoole_http_server("0.0.0.0", 9001);
-
-$http->on("start", function ($server) {
-    echo "Swoole http server is started at http://0.0.0.0:9001\n";
-});
-
-$http->on("request", function ($request, $response) {
-    var_dump($request);
-    $response->header("Content-Type", "text/plain");
-    $response->end("Hello World\n");
-});
-
-$http->start();
+phpinfo();
