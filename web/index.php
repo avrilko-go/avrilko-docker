7d3b3851f6fd0626fec5f60bc73e96576399cc0e
何冰 <hebing@hebingdeMacBook-Air.local>
2019-01-02 13:38:31 +0800

    pcntl 扩展

--- web/index.php
+++ web/index.php
@@ -1,2 +1,15 @@
 <?php
-phpinfo();
+for ($i=0; $i < 10; $i++) {
+    $pid = pcntl_fork();
+    if ($pid == -1) {
+        exit('fork进程失败');
+    } elseif ($pid) {
+        $status = 0;
+        pcntl_wait($status, 1);
+    } else {
+        $pid = getmypid();
+        var_dump($pid);
+        sleep(3);
+        exit();
+    }
+}
